43b78eb43e372f9c98084c4cd31aa42efcf0a7be
Jeremy <jeremy.tomasi@outlook.com>
2018-11-29 21:19:23 +0100

    Ajout du Drag & Drop

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 let liste = document.getElementsByClassName("liste");
 let sections = document.getElementsByTagName("section");
 
+let gameDifficulty = document.getElementsByClassName('difficulty')
+
+console.log(gameDifficulty)
+
 
 function changeMenu(NbBouton) {
     liste[NbBouton].addEventListener('click', function () {
@@ -15,139 +19,35 @@ for (let i = 0; i < liste.length; i++) {
     changeMenu(i)
 }
 
-let mode = document.getElementsByClassName("difficulty");
+let choixPlayer = document.getElementById("choixPlayer");
 
 
-let ordi = document.getElementById("ordi"),
-    humain = document.getElementById("humain"),
-    result = document.getElementById("result");
-// Récupération des images de séléction
-let logos = document.getElementsByClassName("logo");
-
-/* Choix aléatoire  de l'ordinateur entre 1 et 5
-La variable choixAleatoireOrdinateur référence un entier de l'intervalle [0;nombreObjet - 1] */
-
-const nombreObjet = 5;
-
-let tableuObjet = ['laser', 'papier', 'pierre', 'darksathi', 'yoda'];
-
-/*
-Ajouter une valeur dans un tableau
-tableuObjet.push("DarkSathi")
-**/
-let choixAleatoireOrdinateur = Math.floor(nombreObjet * Math.random());
-
-for(let i = 0; i < logos.length;i++){
-    logos[i].setAttribute('onclick',`choicePlayer1(${i})`)
+function DragStart(event){
+    event.dataTransfer.setData('Text',event.target.getAttribute('src'))
 }
 
-function choicePlayer1(){
-    for(let i = 0; i < logos.length;i++){
+document.addEventListener('dragstart',DragStart);
 
-        logos[i].addEventListener('click',function(){
-            return i;
-        })
-    }
+
+function Dragover(event){
+    event.preventDefault();
 }
 
-console.log(choicePlayer1());
-
-let choixHumain = choicePlayer1();
-
-/**
-Math.floor tronque le nombre donné en paramètre
-**/
-console.log(`Choix ordinateur : ${tableuObjet[choixAleatoireOrdinateur]}`);
+choixPlayer.addEventListener('dragover',Dragover)
 
 
-
-//let choixHumain = parseInt(window.prompt("Choisir un nombre entier entre 0 et 2"))
-
-ordi.innerHTML = `<p class='textChoix'>Choix de l'ordinateur :</p> <img src='imgs/${tableuObjet[choixAleatoireOrdinateur]}.png' class='choix'>`;
-
-humain.innerHTML = `<p class='textChoix'>Votre choix :</p> <img src='imgs/${tableuObjet[choixHumain]}.png' class='choix'>`;
-
-console.log(`Choix humain : ${tableuObjet[choixHumain]}`);
-
-
-if (choixHumain == choixAleatoireOrdinateur) {
-    console.log("Partie nulle !");
-    result.innerHTML = "Partie nulle !"
+function Drop(event){
+    event.target.style.backgroundImage = `url(${event.dataTransfer.getData('Text')}`
 }
 
-if (choixHumain > choixAleatoireOrdinateur) {
+choixPlayer.addEventListener('drop',Drop)
 
-    let somme = choixHumain + choixAleatoireOrdinateur;
+let tableauObjet = ["darksathi","laser","pierre","papier","yoda"]
 
+let nbreObjets = tableauObjet.length;
 
-    switch (somme) {
-        case 1:
-            console.log('Ordi gagne');
-            result.innerHTML = "L'ordi a gagné !";
-            break
-        case 2:
-            console.log("Humain gagne");
-            result.innerHTML = "Vous avez gagné !";
-            break
-        case 3:
-            if (choixAleatoireOrdinateur == 0) {
-                console.log('Humain gagne');
-                result.innerHTML = "Vous avez gagné !";
-                break
-            } else {
-                console.log("Ordi gagne");
-                result.innerHTML = "L'ordi a gagné";
-                break
-            }
-        case 4:
-            console.log("Ordi gagne");
-            result.innerHTML = "L'ordi a gagné !";
-            break
-        case 5:
-            console.log('Humain gagne');
-            result.innerHTML = "Vous avez gagné !";
-            break
-    }
-}
+console.log(nbreObjets)
 
-result.style.color = 'white';
+gameDifficulty[0].addEventListener('click',function(){
 
-if (choixHumain < choixAleatoireOrdinateur) {
-
-    let somme = choixHumain + choixAleatoireOrdinateur;
-
-    console.log(`Somme = ${somme}`);
-
-    switch (somme) {
-        case 1:
-            console.log("Humain gagne");
-            result.innerHTML = "Vous avez gagné !";
-            break
-        case 2:
-            console.log("Ordi gagne");
-            result.innerHTML = "L'ordi a gagné";
-            break
-        case 3:
-            if (choixHumain == 0) {
-                console.log("Ordi gagne !");
-                result.innerHTML = "L'ordi a gagné";
-                break
-            } else {
-                console.log("Humain gagne");
-                result.innerHTML = "Vous avez gagné !";
-                break
-            }
-        case 4:
-            console.log("Humain gagne");
-            result.innerHTML = "Vous avez gagné !";
-            break
-        case 5:
-            console.log("Ordi gagne");
-            result.innerHTML = "L'ordi a gagné !";
-            break
-    }
-}
-
-/*for(i in tableuObjet){
-    console.log(tableuObjet[i])
-} */
+})
